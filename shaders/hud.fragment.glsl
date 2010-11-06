ca1e17a6275e603affc296e04c73e32836e24cfa
Joshua Bodine <josh@joshb.us>
2010-11-06 09:25:00 -0700

    Removed redundant shader files.

--- shaders/hud.fragment.glsl
+++
@@ -1,7 +0,0 @@
-#version 110
-
-varying vec4 colorInterpol;
-
-void main() {
-	gl_FragColor = colorInterpol;
-}
--- shaders/text.fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 110
-
-uniform sampler2D fontTexture;
-
-varying vec2 texCoordInterpol;
-
-void main() {
-	gl_FragColor = vec4(1.0, 1.0, 1.0, texture2D(fontTexture, texCoordInterpol).w);
-}
