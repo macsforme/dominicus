2b0ec5428aae6305b9c2680573ce5c332e800fa0
Joshua Bodine <josh@joshb.us>
2010-11-22 03:21:07 -0700

    Added updated cursor drawing shader I forgot to before.

--- shaders/cursor.fragment.glsl
+++ shaders/cursor.fragment.glsl
@@ -5,9 +5,12 @@ uniform float thickness;
 varying vec2 boxCoordInterpol;
 
 void main() {
-	gl_FragColor = (
-			abs(boxCoordInterpol.x) > thickness && abs(boxCoordInterpol.y) > thickness ?
-					vec4(0.0) : vec4(1.0 - distance(vec2(0.0, 0.0), boxCoordInterpol))
-	);
+	float dist = distance(vec2(0.0, 0.0), boxCoordInterpol);
+	vec3 color = vec3(1.0);
+	float minCoord = min(abs(boxCoordInterpol.x), abs(boxCoordInterpol.y));
 
+	if(minCoord < thickness)
+		gl_FragColor = vec4(color, 1.0 - dist);
+	else
+		discard;
 }
