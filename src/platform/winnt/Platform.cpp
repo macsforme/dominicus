e073fae9c974904d647120ff47a59d36437b29c6
Jeffery Myers <JeffM2501@gmail.com>
2011-02-12 12:53:21 -0800

    start platform for windows, base it on the linux one

---
+++ src/platform/winnt/Platform.cpp
@@ -0,0 +1,52 @@
+/*
+ *  Platform.cpp
+ *  dominicus
+ *
+ *  Created by Joshua Bodine on 5/28/10.
+ *  Copyright 2010 Joshua Bodine. All rights reserved.
+ *
+ */
+
+#include "Platform.h"
+
+// platform-dependent includes
+#include <SDL/SDL.h>
+//#include <time.h>
+#include <sys/time.h>
+Platform::Platform() {
+	// determine the resource directory path
+	dataPath = ".";
+
+	// initialize the random number generator
+	srand(time(NULL));
+}
+
+const char* Platform::getArchitecture() {
+	return "Linux";
+}
+
+void Platform::hideCursor() {
+	SDL_ShowCursor(SDL_DISABLE);
+}
+
+void Platform::warpCursor(unsigned int x, unsigned int y) {
+	SDL_WarpMouse(x, y);
+}
+
+unsigned int Platform::getExecutionTimeMicros() {
+	timeval tv;
+	if(gettimeofday(&tv, NULL) == -1)
+		ProgramLog::report(LOG_FATAL, "An error occurred when attempting to retrieve the time.");
+
+	static unsigned long int beginning = tv.tv_sec * 1000000 + tv.tv_usec;
+	unsigned long int now = tv.tv_sec * 1000000 + tv.tv_usec;
+
+	return now - beginning;
+}
+
+void Platform::sleepMicros(unsigned int micros) {
+	timespec delayTime;
+	delayTime.tv_sec = 0;
+	delayTime.tv_nsec = micros * 1000;
+	nanosleep(&delayTime, NULL);
+}
---
+++ src/platform/winnt/main.cpp
@@ -0,0 +1,31 @@
+/*
+ *  main.cpp
+ *  dominicus
+ *
+ *  Created by Joshua Bodine on 10/12/10.
+ *  Copyright 2010 Joshua Bodine. All rights reserved.
+ *
+ */
+
+// program headers
+#include "dominicusMain.h"
+#include "ProgramLog.h"
+
+// library headers
+#include <SDL/SDL.h>
+
+int main(int argc, char* argv[]) {
+	// initialize SDL
+	if(SDL_Init(SDL_INIT_VIDEO) == -1)
+		ProgramLog::report(LOG_FATAL, "SDL could not be initialized.");
+
+	// call the main program routine
+	int returnVal = dominicusMain(argc, argv);
+
+	// destroy SDL
+	SDL_Quit();
+
+	// return
+	return returnVal;
+}
+
