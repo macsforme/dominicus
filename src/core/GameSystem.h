e7c8ff07d2499b5570d8b56d326ce0adda8b7a21
Joshua Bodine <josh@joshb.us>
2014-10-14 05:17:04 -0700

    Version updated to 1 Development.

--- src/core/GameSystem.h
+++ src/core/GameSystem.h
@@ -11,7 +11,7 @@
 #define PROGRAM_VERSION "1"
 
 // Build identifier string (DEVEL | ALPHA# | BETA# | STABLE)
-#define PROGRAM_BUILDSTRING "ALPHA1"
+#define PROGRAM_BUILDSTRING "DEVEL"
 
 #include <cstdlib>
 #include <map>
