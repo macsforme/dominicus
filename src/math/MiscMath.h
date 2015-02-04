d09e127d92e7222112f347464a04e87f5986f2e0
Joshua Bodine <josh@joshb.us>
2015-02-03 22:52:49 -0700

    Re-wrote getAngle() method with more efficient (hopefully) approach, and got rid of some old commented-out math functions we don't use anymore.

--- src/math/MiscMath.h
+++ src/math/MiscMath.h
@@ -4,19 +4,14 @@
 #ifndef MISCMATH_H
 #define MISCMATH_H
 
+#include <cmath>
+
 #include "math/ScalarMath.h"
 #include "math/VectorMath.h"
 
 inline float getAngle(Vector2 point) {
-	if(point.x == 0.0f && point.y == 0.0f)
-		// avoid division by zero
-		return 0.0f;
-
-	Vector2 zeroAngle(1.0f, 0.0f);
-	point.norm();
-	float angle = degrees(acos(dot(zeroAngle, point)));
-	if(point.y > 0.0f)
-		angle = 360.0f - angle;
+	float angle = -degrees(atan2(point.y, point.x));
+	return angle < 0.0f ? angle += 360.0f : angle;
 
 	return angle;
 }
@@ -30,39 +25,6 @@ inline Vector3 getNormal(Vector3 vec0, Vector3 vec1, Vector3 vec2) {
 
 	return normal;
 }
-
-/*
-inline Matrix4 getXVectorTransformationMatrix(Vector4 vector) {
-	Matrix4 transformationMatrix;
-	transformationMatrix.identity();
-
-	rotateMatrix(Vector3(0.0f, 1.0f, 0.0f), radians(getAngle(Vector2(vector.x, vector.z))), transformationMatrix);	// x
-	vector = vector * transformationMatrix;
-	rotateMatrix(Vector3(0.0f, 0.0f, -1.0f), radians(getAngle(Vector2(vector.x, vector.y))), transformationMatrix);	// y
-
-	return transformationMatrix;
-}
-
-inline bool fEq(float a, float b) {
-	return absolute(a - b) < 0.001 ? true : false;
-}
-
-inline Matrix3 completeMatrix(Vector3 vector) {
-	return Matrix3(
-			vector.x,
-			vector.y,
-			vector.z,
-
-			fEq(absolute(vector.y), 1.0f) ? -vector.y : -vector.y * vector.x / sqrt(1.0f - vector.y * vector.y),
-			sqrt(vector.x * vector.x + vector.z * vector.z),
-			fEq(absolute(vector.y), 1.0f) ? 0.0f : -vector.y * vector.z / sqrt(1.0f - vector.y * vector.y),
-
-			fEq(absolute(vector.y), 1.0f) ? 0.0f : -vector.z / sqrt(vector.x * vector.x + vector.z * vector.z),
-			0.0f,
-			fEq(absolute(vector.y), 1.0f) ? 1.0f : vector.x / sqrt(vector.x * vector.x + vector.z * vector.z)
-		);
-}
-*/
 
 inline int roundToInt(float number) {
 	return (number > 0.0f ? number + 0.5f : number - 0.5f);
